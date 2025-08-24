eaceff3e0182052a820f0d6fab2240cd35cae2bb
Sebastian <pe.sebas@gmail.com>
2025-08-23 23:09:54 -0500

    Configured docker-compose to use MySQL database and renamed main class

--- src/main/java/com/pragma/powerup/PlazoletaApplication.java
+++ src/main/java/com/pragma/powerup/PlazoletaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PowerUpApplication {
+public class PlazoletaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PowerUpApplication.class, args);
+		SpringApplication.run(PlazoletaApplication.class, args);
 	}
 
 }
