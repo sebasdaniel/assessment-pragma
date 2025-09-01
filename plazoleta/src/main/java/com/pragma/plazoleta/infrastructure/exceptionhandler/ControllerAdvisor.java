8392fe546790d97c89cbea6a31c2629c55a7bb44
Sebastian <pe.sebas@gmail.com>
2025-09-01 15:53:57 -0500

    Implemented customs exception handler in controller advice class

--- plazoleta/src/main/java/com/pragma/plazoleta/infrastructure/exceptionhandler/ControllerAdvisor.java
+++ plazoleta/src/main/java/com/pragma/plazoleta/infrastructure/exceptionhandler/ControllerAdvisor.java
@@ -1,5 +1,9 @@
 package com.pragma.plazoleta.infrastructure.exceptionhandler;
 
+import com.pragma.plazoleta.domain.exception.DataFormatException;
+import com.pragma.plazoleta.domain.exception.DomainException;
+import com.pragma.plazoleta.domain.exception.ObjectNotFoundException;
+import com.pragma.plazoleta.domain.exception.RequiredDataException;
 import com.pragma.plazoleta.infrastructure.exception.NoDataFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +23,26 @@ public class ControllerAdvisor {
             NoDataFoundException ignoredNoDataFoundException) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(Collections.singletonMap(MESSAGE, ExceptionResponse.NO_DATA_FOUND.getMessage()));
+    }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleObjectNotFoundException(ObjectNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap(MESSAGE, ex.getMessage()));
+    }
+
+    @ExceptionHandler(RequiredDataException.class)
+    public ResponseEntity<Map<String, String>> handleRequiredDataException(RequiredDataException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap(MESSAGE, ex.getMessage()));
+    }
+
+    @ExceptionHandler(DataFormatException.class)
+    public ResponseEntity<Map<String, String>> handleDataFormatException(DataFormatException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap(MESSAGE, ex.getMessage()));
+    }
+
+    @ExceptionHandler(DomainException.class)
+    public ResponseEntity<Map<String, String>> handleDomainException(DomainException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap(MESSAGE, ex.getMessage()));
     }
     
 }
