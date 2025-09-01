9a6de8e0f2389a0dbf3f97abc95842dad795a0cc
Sebastian <pe.sebas@gmail.com>
2025-09-01 17:04:39 -0500

    Added unit test for DishUseCase class

--- plazoleta/src/main/java/com/pragma/plazoleta/domain/model/Dish.java
+++ plazoleta/src/main/java/com/pragma/plazoleta/domain/model/Dish.java
@@ -1,10 +1,12 @@
 package com.pragma.plazoleta.domain.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class Dish {
     private Long id;
     private String name;
