eeda253f5cb3a8805c2faabd8c5ebacde0ee7dd2
Sebastian <pe.sebas@gmail.com>
2025-09-05 16:29:23 -0500

    Implemented validation in dish domain model

--- plazoleta/src/main/java/com/pragma/plazoleta/domain/usecase/DishUseCase.java
+++ plazoleta/src/main/java/com/pragma/plazoleta/domain/usecase/DishUseCase.java
@@ -2,14 +2,21 @@ package com.pragma.plazoleta.domain.usecase;
 
 import com.pragma.plazoleta.domain.api.IDishServicePort;
 import com.pragma.plazoleta.domain.api.IRestaurantServicePort;
+import com.pragma.plazoleta.domain.exception.DomainException;
 import com.pragma.plazoleta.domain.exception.ObjectNotFoundException;
 import com.pragma.plazoleta.domain.exception.RequiredDataException;
 import com.pragma.plazoleta.domain.model.Dish;
 import com.pragma.plazoleta.domain.spi.IDishPersistencePort;
 import lombok.RequiredArgsConstructor;
 
+import static java.util.Objects.isNull;
+
 @RequiredArgsConstructor
 public class DishUseCase implements IDishServicePort {
+
+    private static final String PRICE_EXCEPTION = "Price must be a positive number";
+    private static final String REQUIRED_DATA_EXCEPTION = "One or more required data is not present";
+    private static final String RESTAURANT_NOT_FOUND_EXCEPTION = "The restaurant related with the dish does not exist";
 
     private final IDishPersistencePort dishPersistencePort;
     private final IRestaurantServicePort restaurantServicePort;
@@ -17,11 +24,15 @@ public class DishUseCase implements IDishServicePort {
     @Override
     public void saveDish(Dish dish) {
         if (!haveRequiredData(dish)) {
-            throw new RequiredDataException("One or more required data for dish is not present");
+            throw new RequiredDataException(REQUIRED_DATA_EXCEPTION);
+        }
+
+        if (!isValidDishPrice(dish.getPrice())) {
+            throw new DomainException(PRICE_EXCEPTION);
         }
 
         if (!restaurantServicePort.exist(dish.getRestaurantId())) {
-            throw new ObjectNotFoundException("The restaurant related with the dish does not exist");
+            throw new ObjectNotFoundException(RESTAURANT_NOT_FOUND_EXCEPTION);
         }
 
         dish.setActive(true);
@@ -31,12 +42,15 @@ public class DishUseCase implements IDishServicePort {
 
     @Override
     public Dish updateDish(Dish dish) {
-
-        if (dish.getId() == null || dish.getPrice() == null || dish.getDescription() == null) {
-            throw new RequiredDataException("Required data for update dish is not present");
+        if (isNull(dish.getId()) || isNull(dish.getPrice()) || isNull(dish.getDescription())) {
+            throw new RequiredDataException(REQUIRED_DATA_EXCEPTION);
         }
 
-        var savedDish = dishPersistencePort.getDish(dish.getId());
+        if (!isValidDishPrice(dish.getPrice())) {
+            throw new DomainException(PRICE_EXCEPTION);
+        }
+
+        Dish savedDish = dishPersistencePort.getDish(dish.getId());
         savedDish.setPrice(dish.getPrice());
         savedDish.setDescription(dish.getDescription());
 
@@ -46,5 +60,9 @@ public class DishUseCase implements IDishServicePort {
     private boolean haveRequiredData(Dish dish) {
         return dish.getName() != null && dish.getPrice() != null && dish.getDescription() != null &&
                 dish.getUrlImage() != null && dish.getCategory() != null && dish.getRestaurantId() != null;
+    }
+
+    private boolean isValidDishPrice(Integer price) {
+        return !isNull(price) && price > 0;
     }
 }
