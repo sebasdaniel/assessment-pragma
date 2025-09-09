885fc4c26c9a32ffe88ee4d279ddc2084b508a8d
Sebastian <pe.sebas@gmail.com>
2025-09-09 16:57:46 -0500

    Added permission for get user method because is use by the other service

--- user/src/main/java/com/pragma/plazoleta/user/infrastructure/configuration/SecurityConfig.java
+++ user/src/main/java/com/pragma/plazoleta/user/infrastructure/configuration/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.pragma.plazoleta.user.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -46,6 +47,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .antMatchers("/api/v1/auth/**").permitAll()
+                        .antMatchers(HttpMethod.GET,"/api/v1/user/**").permitAll()
                         .antMatchers("/api/v1/user/owner").hasRole(UserRole.ADMIN_ROLE)
                         .anyRequest().authenticated()
                 );
